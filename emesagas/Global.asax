b91ee032056e92f93a766e9310c5b4ce22a04179
Özgür Eren Erkan <sibereren@gmail.com>
2019-10-23 23:57:07 +0300

    CodeFirstDatabase First Publish

---
+++ emesagas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="emesagas.MvcApplication" Language="C#" %>
---
+++ emesagas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(emesagas.Startup))]
+namespace emesagas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
